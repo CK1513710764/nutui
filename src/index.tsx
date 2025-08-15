79fa16286643fe62187fd6a7b10e5c5ccf6aa9d8
CK1513710764 <chenkai030905@qq.com>
2025-08-15 18:14:16 +0800

    修改

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 export { default as Button } from './components/Button'
 
+export type {
+    ButtonProps,
+    BaseButtonProps,
+    ButtonSize,
+    ButtonType
+} from './components/Button/button';
 
